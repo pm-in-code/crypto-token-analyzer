eb5a86897228ef4905a4b1ff58c94929e85b48c0
Dima <dimabogdan1@mail.ru>
2025-08-28 20:33:50 +0200

    Add spacing between currency name and CMC rating, fix social networks positioning

--- app.js
+++ app.js
@@ -1155,7 +1155,7 @@ const TokenSearch = () => {
       </div>
 
       {/* Trending Tokens Card */}
-      <div className="card">
+      <div className="card relative">
         {/* Social links top-right */}
         <div className="absolute right-6 -top-10 flex gap-3 z-40">
           <a href="#" className="px-4 py-2 rounded-xl bg-amber-50 border border-amber-100 text-gray-800 shadow-sm">Twitter</a>
